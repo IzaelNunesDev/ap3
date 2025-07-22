f7831b0466820231caaa02b87dd99fefb4bdd143
izael <izael@example.com>
2025-07-22 12:24:57 -0300

    fix erros

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.database import connect_to_db_async, disconnect_from_db_async
 from app.models import Rota, Veiculo, Motorista, Aluno, Admin, Viagem, ViagemAlunos
-from caspyorm.models import sync_table_async
 
 # Importa todos os seus arquivos de rota
 from app.routes import alunos, veiculos, motoristas, admins, rotas, viagens, viagem_alunos
@@ -16,13 +15,13 @@ app = FastAPI(
 async def startup_event():
     await connect_to_db_async()
     # Sincroniza todas as tabelas para garantir que o schema esteja correto
-    await sync_table_async(Rota, auto_apply=True)
-    await sync_table_async(Veiculo, auto_apply=True)
-    await sync_table_async(Motorista, auto_apply=True)
-    await sync_table_async(Aluno, auto_apply=True)
-    await sync_table_async(Admin, auto_apply=True)
-    await sync_table_async(Viagem, auto_apply=True)
-    await sync_table_async(ViagemAlunos, auto_apply=True) # <-- CORRIGIDO
+    await Rota.sync_table_async(auto_apply=True)
+    await Veiculo.sync_table_async(auto_apply=True)
+    await Motorista.sync_table_async(auto_apply=True)
+    await Aluno.sync_table_async(auto_apply=True)
+    await Admin.sync_table_async(auto_apply=True)
+    await Viagem.sync_table_async(auto_apply=True)
+    await ViagemAlunos.sync_table_async(auto_apply=True)
 
 @app.on_event("shutdown")
 async def shutdown_event():
